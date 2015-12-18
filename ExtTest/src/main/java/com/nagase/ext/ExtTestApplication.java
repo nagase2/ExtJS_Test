166150ac2c11f1e0c5e2831aee087086326a2a3b
Naggi <nagase2@gmail.com>
2015-12-19 02:53:57 +0900

    Extを追加

--- ExtTest/src/main/java/com/nagase/ext/ExtTestApplication.java
+++ ExtTest/src/main/java/com/nagase/ext/ExtTestApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class ExtTestApplication {
 
+	
     public static void main(String[] args) {
         SpringApplication.run(ExtTestApplication.class, args);
     }
